ad1eb4c46fe3f6c07c32024af40f508d25947ac4
Víkingur Daði <vikingurda@gmail.com>
2026-09-08 12:54:32 +0000

    forritun python

--- forr1/verkefni_16.py
+++ forr1/verkefni_16.py
@@ -1,7 +1,197 @@
+from random import randint
+
 for num1 in range(200, 149, -1):
     print(num1)
 
 for num2 in range(50, 101, +5):
     print(num2)
+q = False
 
-tákn1 = str(input("Sláðu inn tákn: "))
+while q == False:
+    randint_num = randint(1, 37)
+    if randint_num == 1:
+        a = str(input("sláðu á lyklaborðið a: "))
+        if a == "a":
+            print("þú slóst inn a")
+
+    if randint_num == 2:
+            a = str(input("sláðu á lyklaborðið b: "))
+            if a == "b":
+                print("þú slóst inn b")
+
+    if randint_num == 3:
+            a = str(input("sláðu á lyklaborðið c: "))
+            if a == "c":
+                print("þú slóst inn c")
+
+    if randint_num == 4:
+            a = str(input("sláðu á lyklaborðið d: "))
+            if a == "d":
+                print("þú slóst inn d")
+
+    if randint_num == 5:
+            a = str(input("sláðu á lyklaborðið e: "))
+            if a == "e":
+                print("þú slóst inn e")
+
+    if randint_num == 6:
+            a = str(input("sláðu á lyklaborðið f: "))
+            if a == "f":
+                print("þú slóst inn f")
+
+    if randint_num == 7:
+            a = str(input("sláðu á lyklaborðið g: "))
+            if a == "g":
+                print("þú slóst inn g")
+
+    if randint_num == 8:
+            a = str(input("sláðu á lyklaborðið h: "))
+            if a == "h":
+                print("þú slóst inn h")
+
+    if randint_num == 9:
+            a = str(input("sláðu á lyklaborðið i: "))
+            if a == "i":
+                print("þú slóst inn i")
+
+    if randint_num == 10:
+            a = str(input("sláðu á lyklaborðið j: "))
+            if a == "j":
+                print("þú slóst inn j")
+
+    if randint_num == 11:
+            a = str(input("sláðu á lyklaborðið k: "))
+            if a == "k":
+                print("þú slóst inn k")
+
+    if randint_num == 12:
+            a = str(input("sláðu á lyklaborðið l: "))
+            if a == "l":
+                print("þú slóst inn l")
+
+    if randint_num == 13:
+            a = str(input("sláðu á lyklaborðið m: "))
+            if a == "m":
+                print("þú slóst inn m")
+
+    if randint_num == 14:
+            a = str(input("sláðu á lyklaborðið n: "))
+            if a == "n":
+                print("þú slóst inn n")
+
+    if randint_num == 15:
+            a = str(input("sláðu á lyklaborðið o: "))
+            if a == "o":
+                print("þú slóst inn o")
+
+    if randint_num == 16:
+            a = str(input("sláðu á lyklaborðið p: "))
+            if a == "p":
+                print("þú slóst inn p")
+            exit()
+
+    if randint_num == 17:
+            a = str(input("sláðu á lyklaborðið q: "))
+            if a == "q":
+                print("þú slóst inn q")
+                exit()
+
+    if randint_num == 18:
+            a = str(input("sláðu á lyklaborðið r: "))
+            if a == "r":
+                print("þú slóst inn r")
+
+    if randint_num == 19:
+            a = str(input("sláðu á lyklaborðið s: "))
+            if a == "s":
+                print("þú slóst inn s")
+
+    if randint_num == 20:
+            a = str(input("sláðu á lyklaborðið t: "))
+            if a == "t":
+                print("þú slóst inn t")
+
+    if randint_num == 21:
+            a = str(input("sláðu á lyklaborðið u: "))   
+            if a == "u":
+                print("þú slóst inn u")             
+
+    if randint_num == 22:
+            a = str(input("sláðu á lyklaborðið v: "))
+            if a == "v":
+                print("þú slóst inn v")
+
+    if randint_num == 23:
+            a = str(input("sláðu á lyklaborðið w: "))
+            if a == "w":
+                print("þú slóst inn w")
+
+    if randint_num == 24:
+            a = str(input("sláðu á lyklaborðið x: "))
+            if a == "x":
+                print("þú slóst inn x")
+
+    if randint_num == 25:
+            a = str(input("sláðu á lyklaborðið y: "))
+            if a == "y":
+                print("þú slóst inn y")
+
+    if randint_num == 26:
+            a = str(input("sláðu á lyklaborðið z: "))
+            if a == "z":
+                print("þú slóst inn z")   
+
+    if randint_num == 27:
+            a = str(input("sláðu á lyklaborðið 1: "))
+            if a == "1":
+                print("þú slóst inn 1")
+
+    if randint_num == 28:
+            a = str(input("sláðu á lyklaborðið 2: "))
+            if a == "2":
+                print("þú slóst inn 2")
+
+    if randint_num == 29:
+            a = str(input("sláðu á lyklaborðið 3: "))
+            if a == "3":
+                print("þú slóst inn 3")
+
+    if randint_num == 30:
+            a = str(input("sláðu á lyklaborðið 4: "))
+            if a == "4":
+                print("þú slóst inn 4")
+
+    if randint_num == 31:
+            a = str(input("sláðu á lyklaborðið 5: "))
+            if a == "5":
+                print("þú slóst inn 5")
+
+    if randint_num == 32:
+            a = str(input("sláðu á lyklaborðið 6: "))
+            if a == "6":
+                print("þú slóst inn 6")
+
+    if randint_num == 33:
+            a = str(input("sláðu á lyklaborðið 7: "))
+            if a == "7":
+                print("þú slóst inn 7")
+
+    if randint_num == 34:
+            a = str(input("sláðu á lyklaborðið 8: "))
+            if a == "8":
+                print("þú slóst inn 8")
+
+    if randint_num == 35:
+            a = str(input("sláðu á lyklaborðið 9: "))
+            if a == "9":
+                print("þú slóst inn 9")
+
+    if randint_num == 36:
+            a = str(input("sláðu á lyklaborðið 0: "))
+            if a == "0":
+                print("þú slóst inn 0")
+
+def FaedingarAr():
+    return int(input("Hvenær varstu fædd/ur?: "))
+
+print("þú ert ", 2026 - FaedingarAr(), "ára gamall/gömul")
